230dc8df5a1abb5c48fa7607d7e0ff77e6202243
Zimolost <cmorodin046@gmail.com>
2024-07-24 17:20:29 +0300

    first commit

---
+++ elibrary_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import AddBookForm
+from .models import Catalog
+# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        add_book_form = AddBookForm(data=request.POST)
+        if add_book_form.is_valid():
+            add_book_form.save()
+    books = Catalog.objects.all()
+    add_book_form = AddBookForm()
+    return render(request, 'home.html', {'add_book_form': add_book_form, 'books': books})
